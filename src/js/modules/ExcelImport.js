bf58c4d724215b50d7096b1f1776a55ad56b2e3a
Erick M <erickmoreta1221@gmail.com>
2026-06-19 13:50:25 -0400

    fix(import): hacer mas flexible el buscador de columnas de asistencia

--- src/js/modules/ExcelImport.js
+++ src/js/modules/ExcelImport.js
@@ -195,8 +195,8 @@ export const ExcelImport = {
                 if (val.includes("nombre") || val.includes("estudiante") || val.includes("alumno") || val.includes("apellido") || val === "nombres y apellidos") colName = c;
                 if (val === "id" || val.includes("id estudiante")) colID = c;
                 if (val === "no." || val === "no" || val.includes("número") || val === "#") colNo = c;
-                if (val === "% asistencia" || val === "% asist" || val === "% de asistencia") colAtt = c;
-                if (val === "% ausencia" || val === "% aus" || val === "% inasistencia" || val === "% de ausencia") colAbs = c;
+                if (val.includes("%") && val.includes("asis")) colAtt = c;
+                if (val.includes("%") && (val.includes("aus") || val.includes("inasis"))) colAbs = c;
             }
 
             // ID column is optional. The critical identifier is the Student Name column.
